8d6cfb320e1d68ac0a1d0209ebd7ae527c19db27
Endozada <pedro.lucena@dcx.ufpb.br>
2024-09-11 00:07:18 -0300

    Adicionando novo test e novos códigos.

---
+++ src/main/java/br/ufpb/dcx/ayla/agenda/ContatoInexistenteException.java
@@ -0,0 +1,12 @@
+package br.ufpb.dcx.ayla.agenda;
+
+public class ContatoInexistenteException extends Exception {
+
+    public ContatoInexistenteException(String msg) {
+        super(msg);
+    }
+
+    public ContatoInexistenteException() {
+        super("Contato inexistente");
+    }
+}
